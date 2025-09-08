fa726918396ac4993b9a987b1c8bdc5ae00c32a2
Tanu <tanutater@gmail.com>
2025-09-09 00:40:34 +0530

    java

--- video65_exercise_factorial/script.js
+++ video65_exercise_factorial/script.js
@@ -1,8 +1,19 @@
 // calculate factorial using reduce and for loops 
+let a=6;
+function factorial(number){
+  let arr=Array.from(Array(number+1).keys());
+  console.log(arr.slice(1,));
+  let c=arr.slice(1,).reduce((a,b)=>{
+    return a*b;
+  })
+  return c;
+  console.log(c)
+}
+factorial(6);
 console.log("enter a number")
-var a =prompt("enter a number ")
+var b =prompt("enter a number ")
 var product=1;
-for (let index = 1; index <= a; index++) {
+for (let index = 1; index <= b; index++) {
   product=product*index;
 }
 console.log(product)
